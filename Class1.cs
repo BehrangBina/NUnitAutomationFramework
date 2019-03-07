d86f0fca1a194dcee8c60581a135c8f3b0f43616
Behrang Bina <BehrangBina@hotmail.com>
2019-03-07 12:03:22 +1100

    init

---
+++ Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace NUnitAutomationFramework
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void TestAdd()
+        {
+            Assert.True(4+4>0,"4+4>0");
+            Assert.AreEqual(2+2,4,"4");
+        }
+    }
+}
