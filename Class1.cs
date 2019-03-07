8e1ac3b4e8ffa416ec012021d342b1f6381c7242
Behrang Bina <BehrangBina@hotmail.com>
2019-03-07 14:18:39 +1100

    second test added to verify hjenkins

--- Class1.cs
+++ Class1.cs
@@ -16,5 +16,11 @@ namespace NUnitAutomationFramework
             Assert.True(4+4>0,"4+4>0");
             Assert.AreEqual(2+2,4,"4");
         }
+        [Test]
+        public void TestSubtract()
+        {
+            Assert.True(4 - 4 == 0, "4-4==0");
+            Assert.AreEqual(2 -2, 0, "0");
+        }
     }
 }
